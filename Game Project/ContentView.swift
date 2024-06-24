65b385b2666c1244b9b7b4635140974076a025c0
naarmc2 <157539735+naarmc2@users.noreply.github.com>
2024-06-24 09:18:00 -0400

    Initial Commit

---
+++ Game Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Game Project
+//
+//  Created by Nathania McKenzie on 6/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Game Project/Game_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_ProjectApp.swift
+//  Game Project
+//
+//  Created by Nathania McKenzie on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Game_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
